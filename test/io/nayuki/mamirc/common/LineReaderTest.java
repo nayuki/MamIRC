307ecb0995977f5444a2fd2f5e00754c631c8b7d
Nayuki Minase <nayuki@eigenstate.org>
2015-09-20 04:14:13 +0000

    Added test suite for LineReader.

---
+++ test/io/nayuki/mamirc/common/LineReaderTest.java
@@ -0,0 +1,71 @@
+package io.nayuki.mamirc.common;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public final class LineReaderTest {
+	
+	@Test public void testBlank() {
+		test("", "");
+	}
+	
+	@Test public void testOneLine() {
+		test("aa", "aa");
+	}
+	
+	@Test public void testBlankTailCr() {
+		test("b\r", "b", "");
+	}
+	
+	@Test public void testBlankTailLf() {
+		test("b\n", "b", "");
+	}
+	
+	@Test public void testBlankTailCrLf() {
+		test("b\r\n", "b", "");
+	}
+	
+	@Test public void testTwoLinesCr() {
+		test("ba\rcd", "ba", "cd");
+	}
+	
+	@Test public void testTwoLinesLf() {
+		test("ba\ncd", "ba", "cd");
+	}
+	
+	@Test public void testTwoLinesCrLf() {
+		test("ba\r\ncd", "ba", "cd");
+	}
+	
+	@Test public void testAssorted() {
+		test("the\rquick\nbrown\r\nfox\n\njumps\r\n\nover\r\rthelazydog", "the", "quick", "brown", "fox", "", "jumps", "", "over", "", "thelazydog");
+	}
+	
+	@Test public void testLfCr0() {
+		test("ba\n\r", "ba", "", "");
+	}
+	
+	@Test public void testLfCr1() {
+		test("ba\n\rcd", "ba", "", "cd");
+	}
+	
+	
+	private static void test(String raw, String... lines) {
+		try {
+			LineReader reader = new LineReader(new ByteArrayInputStream(raw.getBytes(OutputWriterThread.UTF8_CHARSET)));
+			for (String line : lines) {
+				byte[] actual = reader.readLine();
+				byte[] expected = line.getBytes(OutputWriterThread.UTF8_CHARSET);
+				Assert.assertNotNull(actual != null);
+				Assert.assertArrayEquals(expected, actual);
+			}
+			Assert.assertNull(reader.readLine());
+		} catch (IOException e) {
+			throw new AssertionError(e);
+		}
+	}
+	
+}
