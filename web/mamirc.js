05cea3935d258a20443e26b373f2175e644a95d4
Nayuki Minase <nayuki@eigenstate.org>
2015-09-21 21:08:52 +0000

    Updated web UI JavaScript code to add support for displaying JOIN/PART/QUIT/NICK and properly handling "/me" action messages.

--- web/mamirc.js
+++ web/mamirc.js
@@ -52,28 +52,37 @@ function setActiveWindow(name) {
 		tr.appendChild(td);
 		td = document.createElement("td");
 		var s = data[i][2];
-		while (s != "") {
-			var match = /(^|.*?\()(https?:\/\/[^ )]+)(.*)/.exec(s);
-			if (match == null)
-				match = /(^|.*? )(https?:\/\/[^ ]+)(.*)/.exec(s);
-			if (match == null) {
-				td.appendChild(document.createTextNode(s));
-				break;
-			} else {
-				if (match[1].length > 0)
-					td.appendChild(document.createTextNode(match[1]));
-				var a = document.createElement("a");
-				a.href = match[2];
-				a.target = "_blank";
-				a.appendChild(document.createTextNode(match[2]));
-				td.appendChild(a);
-				s = match[3];
+		var match = ME_REGEX.exec(s);
+		if (match != null) {
+			var em = document.createElement("em");
+			em.appendChild(document.createTextNode(match[1]));
+			td.appendChild(em);
+		} else {
+			while (s != "") {
+				match = /(^|.*?\()(https?:\/\/[^ )]+)(.*)/.exec(s);
+				if (match == null)
+					match = /(^|.*? )(https?:\/\/[^ ]+)(.*)/.exec(s);
+				if (match == null) {
+					td.appendChild(document.createTextNode(s));
+					break;
+				} else {
+					if (match[1].length > 0)
+						td.appendChild(document.createTextNode(match[1]));
+					var a = document.createElement("a");
+					a.href = match[2];
+					a.target = "_blank";
+					a.appendChild(document.createTextNode(match[2]));
+					td.appendChild(a);
+					s = match[3];
+				}
 			}
 		}
 		tr.appendChild(td);
 		messageListElem.appendChild(tr);
 	}
 }
+
+var ME_REGEX = /^\u0001ACTION (.*)\u0001$/;
 
 
 function pollNewMessages() {
@@ -112,9 +121,31 @@ function ingestMessages(data) {
 			var msgs = data[profile].windows[party];
 			for (var i = 0; i < msgs.length; i++) {
 				var s = msgs[i][1];
-				var match = /^PRIVMSG ([^ ]+) (.*)$/.exec(s);
-				if (match != null)
-					messages.push([msgs[i][0], match[1], match[2]]);
+				var parts = split2(s);
+				var who = null;
+				var line = null;
+				if (parts[0] == "PRIVMSG") {
+					var subparts = split2(parts[1]);
+					who = subparts[0];
+					line = subparts[1];
+				} else if (parts[0] == "NICK") {
+					var subparts = split2(parts[1]);
+					who = "*";
+					line = subparts[0] + " changed their name to " + subparts[1];
+				} else if (parts[0] == "JOIN") {
+					who = "*";
+					line = parts[1] + " joined the channel";
+				} else if (parts[0] == "PART") {
+					var subparts = split2(parts[1]);
+					who = "*";
+					line = subparts[0] + " left the channel: " + subparts[1];
+				} else if (parts[0] == "QUIT") {
+					var subparts = split2(parts[1]);
+					who = "*";
+					line = subparts[0] + " has quit: " + subparts[1];
+				}
+				if (who != null && line != null)
+					messages.push([msgs[i][0], who, line]);
 			}
 		}
 		connectionSequences[profile] = [data[profile]["connection-id"], data[profile]["max-sequence"]];
@@ -179,6 +210,14 @@ function twoDigits(n) {
 function removeChildren(elem) {
 	while (elem.firstChild != null)
 		elem.removeChild(elem.firstChild);
+}
+
+
+function split2(str) {
+	var i = str.indexOf(" ");
+	if (i == -1)
+		throw "Cannot split";
+	return [str.substr(0, i), str.substring(i + 1)];
 }
 
 
