000dc80bf569384ab152750a6eef92c179a61581
Nayuki Minase <nayuki@eigenstate.org>
2015-10-07 00:30:19 +0000

    Updated ProcessorReaderThread logic to ignore malformed command lines robustly and to preserve raw bytes in outbound messages.

--- src/io/nayuki/mamirc/connector/ProcessorReaderThread.java
+++ src/io/nayuki/mamirc/connector/ProcessorReaderThread.java
@@ -2,9 +2,9 @@ package io.nayuki.mamirc.connector;
 
 import java.io.IOException;
 import java.net.Socket;
+import java.util.Arrays;
 import io.nayuki.mamirc.common.LineReader;
 import io.nayuki.mamirc.common.OutputWriterThread;
-import io.nayuki.mamirc.common.Utils;
 
 
 final class ProcessorReaderThread extends Thread {
@@ -56,18 +56,33 @@ final class ProcessorReaderThread extends Thread {
 					break;
 				
 				String lineStr = new String(line, "UTF-8");
-				String[] parts = lineStr.split(" ", 5);
+				String[] parts = lineStr.split(" ", 5);  // At most 5 parts
 				String cmd = parts[0];
-				if (cmd.equals("terminate") && parts.length == 1)
-					master.terminateConnector(this);
-				else if (cmd.equals("connect") && parts.length == 5)
-					master.connectServer(parts[1], Integer.parseInt(parts[2]), Boolean.parseBoolean(parts[3]), parts[4], this);
-				else if (cmd.equals("disconnect") && parts.length == 2)
-					master.disconnectServer(Integer.parseInt(parts[1]), this);
-				else if (cmd.equals("send") && parts.length >= 3)
-					master.sendMessage(Integer.parseInt(parts[1]), Utils.toUtf8(lineStr.split(" ", 3)[2]), this);
-				else
-					System.err.println("Unknown line from processor: " + lineStr);
+				
+				try {
+					if (cmd.equals("terminate") && parts.length == 1) {
+						master.terminateConnector(this);
+						
+					} else if (cmd.equals("connect") && parts.length == 5) {
+						if (!(parts[3].equals("true") || parts[3].equals("false")) || parts[4].contains("\0"))
+							throw new IllegalArgumentException();
+						master.connectServer(parts[1], Integer.parseInt(parts[2]), Boolean.parseBoolean(parts[3]), parts[4], this);
+						
+					} else if (cmd.equals("disconnect") && parts.length == 2) {
+						master.disconnectServer(Integer.parseInt(parts[1]), this);
+						
+					} else if (cmd.equals("send") && parts.length >= 3) {
+						byte[] outLine = Arrays.copyOfRange(line, cmd.length() + parts[1].length() + 2, line.length);
+						for (byte b : outLine) {
+							if (b == '\0')
+								throw new IllegalArgumentException();
+						}
+						master.sendMessage(Integer.parseInt(parts[1]), outLine, this);
+						
+					} else {
+						System.err.println("Unknown line from processor: " + lineStr);
+					}
+				} catch (IllegalArgumentException e) {}
 			}
 			
 		// Clean up
