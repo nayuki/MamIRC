a21e6e9e000c3c784b6a43d9e4c25683185a4ea3
Nayuki Minase <nayuki@eigenstate.org>
2016-01-15 21:35:50 +0000

    Added a JavaScript test suite for utilsModule.

---
+++ test/web/mamirc-javascript-test.js
@@ -0,0 +1,111 @@
+/* 
+ * MamIRC
+ * Copyright (c) Project Nayuki
+ * 
+ * http://www.nayuki.io/page/mamirc-the-headless-irc-client
+ * https://github.com/nayuki/MamIRC
+ */
+
+"use strict";
+
+
+(function() {
+	/*---- Initialization ----*/
+	
+	const testMessagesElem = document.getElementById("test-messages");
+	
+	while (testMessagesElem.firstChild != null)
+		testMessagesElem.removeChild(testMessagesElem.firstChild);
+	if (!("utilsModule" in window)) {
+		addMessage("Error: utilsModule not found.");
+		addMessage("Please extract from mamirc.js the entire utilsModule declaration, and put it in a file named utils-module.js.");
+		return;
+	}
+	
+	
+	/*---- Test suite definitions ----*/
+	
+	function testNthRemainingPart() {
+		var f = utilsModule.nthRemainingPart;
+		check(f, "a b c", "a b c", 0);
+		check(f, "b c"  , "a b c", 1);
+		check(f, "c"    , "a b c", 2);
+		check(f, " yz", "x  yz", 1);
+		check(f, "yz" , "x  yz", 2);
+	}
+	
+	
+	function testCountUtf8Bytes() {
+		var f = utilsModule.countUtf8Bytes;
+		check(f, 0, "");
+		check(f, 1, "a");
+		check(f, 4, " |#Z");
+		check(f, 15, "\u3068\u3082\u3048\u30DE\u30DF");
+		check(f, 4, "\uD83D\uDE04");
+		check(f, 15, "\uD800xy\uDC00\uD900z\uDA00");
+	}
+	
+	
+	function testIsChannelName() {
+		var f = utilsModule.isChannelName;
+		check(f, false, "Alice");
+		check(f, true, "#bob");
+		check(f, true, "##carol");
+		check(f, true, "&dave");
+		check(f, false, "eve#");
+		check(f, false, " #fred");
+	}
+	
+	
+	function testTwoDigits() {
+		var f = utilsModule.twoDigits;
+		check(f, "00",  0);
+		check(f, "01",  1);
+		check(f, "09",  9);
+		check(f, "10", 10);
+		check(f, "37", 37);
+		check(f, "99", 99);
+	}
+	
+	
+	const TEST_SUITE = [
+		testNthRemainingPart,
+		testCountUtf8Bytes,
+		testIsChannelName,
+		testTwoDigits,
+	];
+	
+	
+	/*---- Main test runner ----*/
+	
+	TEST_SUITE.forEach(function(func) {
+		try {
+			func();
+			addMessage(func.name + ": Pass");
+		} catch (e) {
+			addMessage(func.name + ": Fail (" + e + ")");
+		}
+	});
+	addMessage("Test completed");
+	
+	
+	/*---- Helper functions ----*/
+	
+	function check(func, expectResult) {  // Expects extra arguments
+		if (arguments.length < 2)
+			throw "Invalid number of arguments";
+		var args = [];
+		for (var i = 2; i < arguments.length; i++)
+			args.push(arguments[i]);
+		var actualResult = func.apply(null, args);
+		if (actualResult !== expectResult)
+			throw "Value mismatch: " + actualResult + " !== " + expectResult;
+	}
+	
+	function addMessage(str) {
+		var li = document.createElement("li");
+		li.appendChild(document.createTextNode(str));
+		testMessagesElem.appendChild(li);
+	}
+	
+})();
