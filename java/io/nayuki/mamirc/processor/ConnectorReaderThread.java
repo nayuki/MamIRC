3a00c68ff3f709299a178deb0c7050ed6405da89
Project Nayuki <me@nayuki.io>
2016-06-04 22:59:00 +0000

    Re-added, rearranged, and modified the logic in the Processor for connecting to the Connector.

---
+++ java/io/nayuki/mamirc/processor/ConnectorReaderThread.java
@@ -0,0 +1,127 @@
+/* 
+ * MamIRC
+ * Copyright (c) Project Nayuki
+ * 
+ * https://www.nayuki.io/page/mamirc-the-headless-irc-client
+ * https://github.com/nayuki/MamIRC
+ */
+
+package io.nayuki.mamirc.processor;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Map;
+import io.nayuki.mamirc.common.BackendConfiguration;
+import io.nayuki.mamirc.common.CleanLine;
+import io.nayuki.mamirc.common.Event;
+import io.nayuki.mamirc.common.LineReader;
+import io.nayuki.mamirc.common.OutputWriterThread;
+import io.nayuki.mamirc.common.Utils;
+import io.nayuki.mamirc.common.WorkerThread;
+
+
+/* 
+ * A worker thread that reads event lines from a socket connection to the MamIRC Connector.
+ * Additional functionality:
+ * - Authenticates with the connector
+ * - Parses the list of current active connections
+ * - Creates and terminates a writer thread for the socket
+ */
+final class ConnectorReaderThread extends WorkerThread {
+	
+	/*---- Fields ----*/
+	
+	private final MamircProcessor master;
+	private Socket socket;
+	private LineReader reader;
+	private OutputWriterThread writer;
+	
+	
+	
+	/*---- Constructor ----*/
+	
+	public ConnectorReaderThread(MamircProcessor master, BackendConfiguration config) throws IOException {
+		super("ConnectorReaderThread");
+		if (master == null || config == null)
+			throw new NullPointerException();
+		this.master = master;
+		
+		// Connect and authenticate
+		socket = new Socket("localhost", config.connectorServerPort);
+		Utils.logger.info("Socket opened to " + socket.getInetAddress() + " port " + socket.getPort());
+		writer = new OutputWriterThread(socket.getOutputStream(), new byte[]{'\n'});
+		writer.start();
+		writer.postWrite(new CleanLine(config.getConnectorPassword(), false));
+		writer.postWrite(new CleanLine("attach"));
+		
+		// Read first line
+		reader = new LineReader(socket.getInputStream());
+		String line = readStringLine(reader);
+		if (line == null)
+			throw new RuntimeException("Authentication failure");
+		if (!line.equals("active-connections"))
+			throw new RuntimeException("Invalid data format");
+		Utils.logger.info("Connector connection successfully authenticated");
+	}
+		
+	
+	
+	/*---- Methods ----*/
+	
+	public OutputWriterThread readInitialDataAndGetWriter(Map<Integer,Integer> connectionSequences) throws IOException {
+		// Read the set of current connections
+		while (true) {
+			String line = readStringLine(reader);
+			if (line.equals("end-list"))
+				break;
+			String[] parts = line.split(" ", 2);  // Connection ID, next (unused) sequence number
+			connectionSequences.put(Integer.parseInt(parts[0]), Integer.parseInt(parts[1]));
+		}
+		String line = readStringLine(reader);
+		if (!line.equals("live-events"))
+			throw new RuntimeException("Invalid data format");
+		Utils.logger.fine("Received current set of connections: " + connectionSequences);
+		return writer;
+	}
+	
+	
+	public void runInner() throws IOException {
+		try {
+			// Read and parse real-time events
+			while (true) {
+				String line = readStringLine(reader);
+				if (line == null)
+					break;
+				Utils.logger.finest("Received event from Connector");
+				String[] parts = line.split(" ", 5);
+				Event ev = new Event(
+					Integer.parseInt(parts[0]),
+					Integer.parseInt(parts[1]),
+					Long.parseLong(parts[2]),
+					Event.Type.fromOrdinal(Integer.parseInt(parts[3])),
+					new CleanLine(parts[4]));
+				master.processEvent(ev);
+			}
+		}
+		finally {  // Clean up
+			writer.terminate();
+			if (socket != null) {
+				try {
+					socket.close();
+				} catch (IOException e) {}
+			}
+			master.terminateProcessor("Lost connection to Connector");
+		}
+	}
+	
+	
+	// Returns the next line from the given reader decoded as UTF-8, or null if the end of stream is reached.
+	private static String readStringLine(LineReader reader) throws IOException {
+		byte[] line = reader.readLine();
+		if (line == LineReader.BLANK_EOF || line == null)
+			return null;
+		else
+			return Utils.fromUtf8(line);
+	}
+	
+}
--- java/io/nayuki/mamirc/processor/MamircProcessor.java
+++ java/io/nayuki/mamirc/processor/MamircProcessor.java
@@ -4,13 +4,21 @@ import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.logging.ConsoleHandler;
 import java.util.logging.Formatter;
 import java.util.logging.Handler;
 import java.util.logging.Level;
 import java.util.logging.LogRecord;
 import java.util.logging.Logger;
+import com.almworks.sqlite4java.SQLiteConnection;
+import com.almworks.sqlite4java.SQLiteException;
+import com.almworks.sqlite4java.SQLiteStatement;
 import io.nayuki.mamirc.common.BackendConfiguration;
+import io.nayuki.mamirc.common.CleanLine;
+import io.nayuki.mamirc.common.Event;
+import io.nayuki.mamirc.common.OutputWriterThread;
 import io.nayuki.mamirc.common.Utils;
 
 
@@ -18,7 +26,7 @@ public final class MamircProcessor {
 	
 	/*---- Stub main program ----*/
 	
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, SQLiteException {
 		// Check the argument count
 		if (args.length != 1) {
 			System.err.println("Usage: java io/nayuki/mamirc/processor/MamircProcessor BackendConfig.json");
@@ -67,11 +75,71 @@ public final class MamircProcessor {
 	
 	
 	
+	/*---- Fields (global state) ----*/
+	
+	private ConnectorReaderThread reader;
+	private OutputWriterThread writer;
+	private boolean isEventRealtime;
+	
+	
+	
 	/*---- Constructor ----*/
 	
-	public MamircProcessor(BackendConfiguration backendConfig) {
+	public MamircProcessor(BackendConfiguration backendConfig) throws IOException, SQLiteException {
 		if (backendConfig == null)
 			throw new NullPointerException();
+		
+		reader = new ConnectorReaderThread(this, backendConfig);
+		Map<Integer,Integer> connectionSequences = new HashMap<>();
+		writer = reader.readInitialDataAndGetWriter(connectionSequences);
+		processExistingConnections(backendConfig.connectorDatabaseFile, connectionSequences);
+		reader.start();
+	}
+	
+	
+	
+	/*---- Methods ----*/
+	
+	private void processExistingConnections(File dbFile, Map<Integer,Integer> connectionSequences) throws SQLiteException {
+		// Read archived events from database and process them
+		isEventRealtime = false;
+		SQLiteConnection database = new SQLiteConnection(dbFile);
+		try {
+			database.open(false);
+			SQLiteStatement query = database.prepare(
+				"SELECT sequence, timestamp, type, data FROM events " +
+				"WHERE connectionId=? AND sequence<? ORDER BY sequence ASC");
+			for (Map.Entry<Integer,Integer> entry : connectionSequences.entrySet()) {
+				int conId = entry.getKey();
+				int nextSeq = entry.getValue();
+				query.bind(1, conId);
+				query.bind(2, nextSeq);
+				while (query.step()) {
+					Event ev = new Event(
+						conId,
+						query.columnInt(0),
+						query.columnLong(1),
+						Event.Type.fromOrdinal(query.columnInt(2)),
+						new CleanLine(query.columnBlob(3), false));
+					processEvent(ev);  // Non-real-time
+				}
+				query.reset();
+			}
+		} finally {
+			database.dispose();  // Automatically disposes its associated statements
+		}
+		
+		isEventRealtime = true;
+	}
+	
+	
+	synchronized void processEvent(Event ev) {}
+	
+	
+	synchronized void terminateProcessor(String reason) {
+		Utils.logger.info("MamIRC Processor application terminating");
+		System.exit(1);
+		throw new AssertionError("Unreachable");
 	}
 	
 }
