c08bbcc80620728edd63460e18d6fd945632109d
Project Nayuki <me@nayuki.io>
2016-08-26 18:39:04 +0000

    Completely revamped static file serving logic in WebServer, added member-level comments.

--- java/io/nayuki/mamirc/processor/WebServer.java
+++ java/io/nayuki/mamirc/processor/WebServer.java
@@ -14,6 +14,9 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.InetSocketAddress;
 import java.nio.file.Files;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import com.almworks.sqlite4java.SQLiteException;
@@ -24,34 +27,33 @@ import io.nayuki.json.Json;
 import io.nayuki.mamirc.common.Utils;
 
 
-public class WebServer {
+final class WebServer {
 	
+	/*---- Fields ----*/
+	
+	// Maps a path to a file and media type, e.g. "/foo.png" -> (File("/bin/mamirc/web/foo.png"), "image/png").
+	private Map<String,Object[]> authorizedStaticFiles;
+	
+	
+	
+	/*---- Constructors ----*/
+	
+	// Constructs and starts a web server with the given arguments.
 	public WebServer(int port, final MamircProcessor master, final MessageManager msgMgr) throws IOException {
+		scanAuthorizedStaticFiles(new File("web"));
 		HttpServer server = HttpServer.create(new InetSocketAddress("localhost", port), 0);
 		
-		server.createContext("/log-viewer", new HttpHandler() {
+		// Mainly handles all static files, plus miscellaneous
+		server.createContext("/", new HttpHandler() {
 			public void handle(HttpExchange he) throws IOException {
-				he.getResponseHeaders().set("Content-Type", "application/xhtml+xml");
-				he.sendResponseHeaders(200, 0);
-				Files.copy(new File("web/log-viewer.html").toPath(), he.getResponseBody());
-				he.close();
-			}
-		});
-		
-		server.createContext("/log-viewer.css", new HttpHandler() {
-			public void handle(HttpExchange he) throws IOException {
-				he.getResponseHeaders().set("Content-Type", "text/css");
-				he.sendResponseHeaders(200, 0);
-				Files.copy(new File("web/log-viewer.css").toPath(), he.getResponseBody());
-				he.close();
-			}
-		});
-		
-		server.createContext("/log-viewer.js", new HttpHandler() {
-			public void handle(HttpExchange he) throws IOException {
-				he.getResponseHeaders().set("Content-Type", "application/javascript");
-				he.sendResponseHeaders(200, 0);
-				Files.copy(new File("web/log-viewer.js").toPath(), he.getResponseBody());
+				Object[] info = authorizedStaticFiles.get(he.getRequestURI().getPath());
+				if (info != null) {
+					he.getResponseHeaders().set("Content-Type", (String)info[1]);
+					byte[] data = readFile((File)info[0]);
+					he.sendResponseHeaders(200, data.length > 0 ? data.length : -1);
+					he.getResponseBody().write(data);
+				} else
+					he.sendResponseHeaders(404, -1);
 				he.close();
 			}
 		});
@@ -102,33 +104,6 @@ public class WebServer {
 			}
 		});
 		
-		server.createContext("/network-profiles", new HttpHandler() {
-			public void handle(HttpExchange he) throws IOException {
-				he.getResponseHeaders().set("Content-Type", "application/xhtml+xml");
-				he.sendResponseHeaders(200, 0);
-				Files.copy(new File("web/network-profiles.html").toPath(), he.getResponseBody());
-				he.close();
-			}
-		});
-		
-		server.createContext("/network-profiles.css", new HttpHandler() {
-			public void handle(HttpExchange he) throws IOException {
-				he.getResponseHeaders().set("Content-Type", "text/css");
-				he.sendResponseHeaders(200, 0);
-				Files.copy(new File("web/network-profiles.css").toPath(), he.getResponseBody());
-				he.close();
-			}
-		});
-		
-		server.createContext("/network-profiles.js", new HttpHandler() {
-			public void handle(HttpExchange he) throws IOException {
-				he.getResponseHeaders().set("Content-Type", "application/javascript");
-				he.sendResponseHeaders(200, 0);
-				Files.copy(new File("web/network-profiles.js").toPath(), he.getResponseBody());
-				he.close();
-			}
-		});
-		
 		server.createContext("/get-network-profiles.json", new HttpHandler() {
 			public void handle(HttpExchange he) throws IOException {
 				Object respData = master.getNetworkProfilesAsJson();
@@ -143,6 +118,59 @@ public class WebServer {
 		ExecutorService executor = Executors.newFixedThreadPool(10);
 		server.setExecutor(executor);
 		server.start();
+	}
+	
+	
+	
+	/*---- Methods ----*/
+	
+	private void scanAuthorizedStaticFiles(File rootDir) {
+		authorizedStaticFiles = new HashMap<>();
+		for (File item : rootDir.listFiles()) {
+			if (!item.isFile())
+				continue;
+			String name = item.getName();
+			if (name.indexOf('.') == -1)
+				continue;
+			String ext = name.substring(name.lastIndexOf('.') + 1);
+			String type = EXTENSION_TO_MEDIA_TYPE.get(ext);
+			if (type == null)
+				continue;
+			if (name.endsWith(".html"))
+				name = name.substring(0, name.length() - ".html".length());
+			authorizedStaticFiles.put("/" + name, new Object[]{item, type});
+		}
+	}
+	
+	
+	
+	/*---- Static members ----*/
+	
+	// Maps a lowercase file extension to an Internet media type.
+	private static final Map<String,String> EXTENSION_TO_MEDIA_TYPE;
+	static {
+		String[][] mapping = {
+			{"css" , "text/css"},
+			{"gif" , "image/gif"},
+			{"html", "application/xhtml+xml; charset=UTF-8"},
+			{"jpg" , "image/jpeg"},
+			{"jpeg", "image/jpeg"},
+			{"js"  , "application/javascript"},
+			{"png" , "image/png"},
+			{"svg" , "image/svg+xml"},
+		};
+		Map<String,String> map = new HashMap<>();
+		for (String[] pair : mapping)
+			map.put(pair[0], pair[1]);
+		EXTENSION_TO_MEDIA_TYPE = Collections.unmodifiableMap(map);
+	}
+	
+	
+	// Returns the full contents of the given file as a byte array.
+	private static byte[] readFile(File file) throws IOException {
+		ByteArrayOutputStream bout = new ByteArrayOutputStream();
+		Files.copy(file.toPath(), bout);
+		return bout.toByteArray();
 	}
 	
 }
