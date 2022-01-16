ae6e0d0d37dfed1055affedf7bba976e1cde3313
Project Nayuki <me@nayuki.io>
2022-01-16 07:40:34 +0000

    Capitalized a keyword in SQL code.

--- server/resource/mamirc-database-init.sql
+++ server/resource/mamirc-database-init.sql
@@ -24,8 +24,8 @@ CREATE TABLE profile_servers(
 	profile_id          INTEGER NOT NULL  REFERENCES irc_network_profiles   ,
 	ordering            INTEGER NOT NULL                                    ,
 	hostname            TEXT    NOT NULL                                    ,
-	port                INTEGER NOT NULL  check(port between 0 and 65535)   ,
-	tls_mode            INTEGER NOT NULL  check(tls_mode between 0 and 2)   ,
+	port                INTEGER NOT NULL  CHECK(port between 0 and 65535)   ,
+	tls_mode            INTEGER NOT NULL  CHECK(tls_mode between 0 and 2)   ,
 	PRIMARY KEY(profile_id, ordering)                                       )
 
 CREATE TABLE profile_nicknames(
@@ -47,7 +47,7 @@ CREATE TABLE connections(
 
 CREATE TABLE connection_events(
 	connection_id      INTEGER NOT NULL  REFERENCES connections,
-	sequence           INTEGER NOT NULL  check(sequence >= 0)  ,
+	sequence           INTEGER NOT NULL  CHECK(sequence >= 0)  ,
 	timestamp_unix_ms  INTEGER NOT NULL                        ,
 	data               BLOB    NOT NULL                        ,
 	PRIMARY KEY(connection_id, sequence)                       )
@@ -63,7 +63,7 @@ CREATE INDEX message_windows_index_0 ON message_windows(
 
 CREATE TABLE processed_messages(
 	window_id  INTEGER NOT NULL  REFERENCES message_windows,
-	sequence   INTEGER NOT NULL  check(sequence >= 0)      ,
+	sequence   INTEGER NOT NULL  CHECK(sequence >= 0)      ,
 	data       TEXT    NOT NULL                            ,
 	PRIMARY KEY(window_id, sequence)                       )
 
