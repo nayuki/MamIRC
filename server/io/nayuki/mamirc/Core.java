a17537e8854a148505754cfcbd0367890d276fdc
Project Nayuki <me@nayuki.io>
2022-01-17 02:58:39 +0000

    Added data structure to capture more information when Core.postEvent() is called.

--- server/io/nayuki/mamirc/Core.java
+++ server/io/nayuki/mamirc/Core.java
@@ -8,14 +8,25 @@ import java.util.concurrent.Semaphore;
 
 final class Core {
 	
-	private Queue<ConnectionEvent> eventQueue = new ConcurrentLinkedQueue<>();
+	private Queue<AugmentedConnectionEvent> eventQueue = new ConcurrentLinkedQueue<>();
 	private Semaphore eventQueueLength = new Semaphore(0);
 	
 	
 	public void postEvent(IrcServerConnection con, ConnectionEvent ev) {
-		Objects.requireNonNull(con);
-		eventQueue.add(Objects.requireNonNull(ev));
+		AugmentedConnectionEvent ace = new AugmentedConnectionEvent();
+		ace.connection = Objects.requireNonNull(con);
+		ace.event = Objects.requireNonNull(ev);
+		eventQueue.add(ace);
 		eventQueueLength.release();
+	}
+	
+	
+	
+	private static final class AugmentedConnectionEvent {
+		
+		public IrcServerConnection connection;
+		public ConnectionEvent event;
+		
 	}
 	
 }
