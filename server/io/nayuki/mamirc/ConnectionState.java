0881fd4334c6622d42914b6417846cb567873856
Project Nayuki <me@nayuki.io>
2022-01-19 03:30:45 +0000

    Added basic connection event handlers.

--- server/io/nayuki/mamirc/ConnectionState.java
+++ server/io/nayuki/mamirc/ConnectionState.java
@@ -1,19 +1,52 @@
 package io.nayuki.mamirc;
 
+import java.nio.charset.Charset;
+
 
 final class ConnectionState {
 	
 	public final long connectionId;
 	public final IrcNetworkProfile profile;
+	private final Charset charset;
+	
 	
 	
 	public ConnectionState(long connectionId, IrcNetworkProfile profile) {
 		this.connectionId = connectionId;
 		this.profile = profile;
+		charset = Charset.forName(profile.characterEncoding);
 	}
 	
 	
 	public void handle(ConnectionEvent ev, IrcServerConnection con) {
+		if (ev instanceof ConnectionEvent.Opened) {
+			send(con, "NICK", profile.nicknames.get(0));
+			send(con, "USER", profile.username, "0", "*", profile.realName);
+		}
+		
+		else if (ev instanceof ConnectionEvent.LineReceived) {
+			String line = new String(((ConnectionEvent.LineReceived)ev).line, charset);
+			IrcMessage msg;
+			try {
+				msg = IrcMessage.parseLine(line);
+			} catch (IllegalArgumentException e) {
+				return;
+			}
+			
+			switch (msg.command) {
+				case "PING": {
+					if (msg.parameters.size() == 1)
+						send(con, "PONG", msg.parameters.get(0));
+					break;
+				}
+			}
+		}
+	}
+	
+	
+	private void send(IrcServerConnection con, String cmd, String... params) {
+		IrcMessage msg = IrcMessage.makeWithoutPrefix(cmd, params);
+		con.postWriteLine(msg.toString().getBytes(charset));
 	}
 	
 }
