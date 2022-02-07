ad645c552417410c01304678a4e85536914578dd
Project Nayuki <me@nayuki.io>
2022-02-07 02:32:31 +0000

    Added semicolons to SQL statements, making the script executable in other environments.

--- server/resource/database-initialization.sql
+++ server/resource/database-initialization.sql
@@ -1,24 +1,24 @@
-SELECT '==== This is a custom text format where each consecutive pair of SQL statements is separated by two newline sequences. It is because the chosen SQLite JDBC driver does not support executing multiple statements in one call. ===='
+SELECT '==== This is a custom text format where each consecutive pair of SQL statements is separated by two newline sequences. It is because the chosen SQLite JDBC driver does not support executing multiple statements in one call. ====';
 
-PRAGMA journal_mode = WAL
+PRAGMA journal_mode = WAL;
 
-BEGIN TRANSACTION
+BEGIN TRANSACTION;
 
 CREATE TABLE configuration(
 	key    TEXT NOT NULL  PRIMARY KEY,
-	value  TEXT NOT NULL             )
+	value  TEXT NOT NULL             );
 
 CREATE TABLE irc_network_profiles(
 	profile_id    INTEGER NOT NULL  PRIMARY KEY,
 	profile_name  TEXT    NOT NULL             ,
-	UNIQUE(profile_name)                       )
+	UNIQUE(profile_name)                       );
 
 CREATE TABLE profile_configuration(
 	profile_id          INTEGER NOT NULL  PRIMARY KEY REFERENCES irc_network_profiles,
 	do_connect          INTEGER NOT NULL  CHECK(do_connect in (0,1))                 ,
 	username            TEXT    NOT NULL                                             ,
 	real_name           TEXT    NOT NULL                                             ,
-	character_encoding  TEXT    NOT NULL                                             )
+	character_encoding  TEXT    NOT NULL                                             );
 
 CREATE TABLE profile_servers(
 	profile_id          INTEGER NOT NULL  REFERENCES irc_network_profiles,
@@ -26,40 +26,40 @@ CREATE TABLE profile_servers(
 	hostname            TEXT    NOT NULL                                 ,
 	port                INTEGER NOT NULL  CHECK(port between 0 and 65535),
 	tls_mode            INTEGER NOT NULL  CHECK(tls_mode between 0 and 2),
-	PRIMARY KEY(profile_id, ordering)                                    )
+	PRIMARY KEY(profile_id, ordering)                                    );
 
 CREATE TABLE profile_nicknames(
 	profile_id          INTEGER NOT NULL  REFERENCES irc_network_profiles,
 	ordering            INTEGER NOT NULL                                 ,
 	nickname            TEXT    NOT NULL                                 ,
 	PRIMARY KEY(profile_id, ordering)                                    ,
-	UNIQUE(profile_id, nickname)                                         )
+	UNIQUE(profile_id, nickname)                                         );
 
 CREATE TABLE profile_after_registration_commands(
 	profile_id          INTEGER NOT NULL  REFERENCES irc_network_profiles,
 	ordering            INTEGER NOT NULL                                 ,
 	command             TEXT    NOT NULL                                 ,
-	PRIMARY KEY(profile_id, ordering)                                    )
+	PRIMARY KEY(profile_id, ordering)                                    );
 
 CREATE TABLE connections(
 	connection_id  INTEGER NOT NULL  PRIMARY KEY                    ,
-	profile_id     INTEGER NOT NULL  REFERENCES irc_network_profiles)
+	profile_id     INTEGER NOT NULL  REFERENCES irc_network_profiles);
 
 CREATE TABLE connection_events(
 	connection_id      INTEGER NOT NULL  REFERENCES connections,
 	sequence           INTEGER NOT NULL  CHECK(sequence >= 0)  ,
 	timestamp_unix_ms  INTEGER NOT NULL                        ,
 	data               BLOB    NOT NULL                        ,
-	PRIMARY KEY(connection_id, sequence)                       )
+	PRIMARY KEY(connection_id, sequence)                       );
 
 CREATE TABLE message_windows(
 	window_id       INTEGER NOT NULL  PRIMARY KEY                    ,
 	profile_id      INTEGER NOT NULL  REFERENCES irc_network_profiles,
 	display_name    TEXT    NOT NULL                                 ,
-	canonical_name  TEXT    NOT NULL                                 )
+	canonical_name  TEXT    NOT NULL                                 );
 
 CREATE INDEX message_windows_index_0 ON message_windows(
-	profile_id, canonical_name, window_id)
+	profile_id, canonical_name, window_id);
 
 CREATE TABLE processed_messages(
 	window_id          INTEGER NOT NULL  REFERENCES message_windows,
@@ -67,6 +67,6 @@ CREATE TABLE processed_messages(
 	timestamp_unix_ms  INTEGER NOT NULL                            ,
 	data               TEXT    NOT NULL                            ,
 	marked_read        INTEGER NOT NULL  CHECK(marked_read in(0,1)),
-	PRIMARY KEY(window_id, sequence)                               )
+	PRIMARY KEY(window_id, sequence)                               );
 
-COMMIT TRANSACTION
+COMMIT TRANSACTION;
